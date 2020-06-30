e1000539ef29853de150616ab2bb31041d3d752b
Agustin Forero <agforero@gmail.com>
2020-06-30 23:37:24 +0000

    even bettergit add scraping/findRelevant.pygit add scraping/findRelevant.py

--- scraping/findRelevant.py
+++ scraping/findRelevant.py
@@ -32,7 +32,8 @@ def main():
         printUsage()
         print("Where:\t-help or -h displays options,")
         print("\t-f returns only filenames with no extension,")
-        print("\t-o returns filenames with \'.o\' appended, and")
+        print("\t-o returns filenames with \'.o\' appended,")
+        print("\t-x returns filenames with \'.exe\' appended, and ")
         print("\t-e returns filenames with extensions kept.")
         print("\t<extension> designates the extension to search for, and\n\t(exclusions) are files to be excluded from output.")
         return 0
@@ -45,6 +46,9 @@ def main():
 
         elif sys.argv[1] == "-o": # with .o added
             disp = disp + file[:0-len(sys.argv[2])] + ".o "
+
+        elif sys.argv[1] == "-x": # with .exe appended
+            disp = disp + file[:0-len(sys.argv[2])] + ".exe "
 
         elif sys.argv[1] == "-e": # with extension maintained
             disp = disp + file + " "
